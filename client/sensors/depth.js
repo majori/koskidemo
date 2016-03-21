fbf2187c717dde71c8694e4659b77e1d92ace08c
Oskari Burri <oskari.burri@student.tut.fi>
2016-03-21 19:18:09 +0200

    code working in theory, needs RL testing

--- client/sensors/depth.js
+++ client/sensors/depth.js
@@ -1,47 +1,133 @@
-"use strict";
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+// Leave the above lines for propper jshinting
+//Type Node.js Here :)
 
 var mraa = require('mraa');
-var Promise = require('bluebird')
+var async = require('async');
 
-const var TIMEOUT_MS = 2000
-const var TIMEOUT = -2
+var TIMEOUT_MS = 2000;
+var TIMEOUT = -2;
 
-class depth {
+function depth(RecievePin, SendPin) {
 
-	constructor(RecievePin, SendPin) {
-		this.myRecievePin = new mraa.Gpio(RecievePin);
-		this.mySendPin = new mraa.Gpio(SendPin);
-		var timer = 0;
-	}
-
-	getRawValue(){
-
-		mySendPin.write(0);
-		then.myRecievePin.dir(mraa.DIR_IN);
-		then.myRecievePin.dir(mraa.DIR_OUT);
-		then.myRecievePin.write(0);
-		then.setTimeout(function(){
-			myRecievePin.dir(mraa.DIR_IN);
-			mySendPin.write(1);
-		}, 25)
-		then.while(myRecievePin.read() = 0 && (timer < TIMEOUT_MS)){
-			++timer;
-		}
-		then.if(timer >= TIMEOUT_MS){
-			return TIMEOUT;
-		}
-		
-		then.myRecievePin.dir(mraa.DIR_OUT)  // receivePin to OUTPUT - pin is now HIGH AND OUTPUT
-		then.myRecievePin.write(1);
-		then.myRecievePin.write(1);
-		then.myRecievePin.write(0);
-		then.myRecievePin.dir(mraa.DIR_IN) 	// receivePin to INPUT (pullup is off)
-
-	}
-
+    this.myRecievePin = new mraa.Gpio(RecievePin);
+    
+    this.mySendPin = new mraa.Gpio(SendPin);
 
 
 }
 
+depth.prototype.getValue = function () {
 
+    var that = this;
+    var timer = 0;
+    console.log("Rec: " + this.myRecievePin.read() + "Send: " + this.mySendPin.read() );
+    
+    async.series([
+        function (callback) {
 
+            that.mySendPin.dir(mraa.DIR_OUT);
+            callback(null, 1);
+        },
+        function (callback) {
+
+            that.mySendPin.write(0);
+            console.log("Send status: " + that.mySendPin.read());
+            callback(null, 2);
+        },
+        function (callback) {
+
+            that.myRecievePin.dir(mraa.DIR_IN);
+            callback(null, 3);
+        },
+        function (callback) {
+
+            that.myRecievePin.dir(mraa.DIR_IN);
+            callback(null, 4);
+        },
+        function (callback) {
+
+            that.myRecievePin.dir(mraa.DIR_IN);
+            that.myRecievePin.dir(mraa.DIR_OUT);
+            callback(null, 5);
+        },
+        function (callback) {
+
+            that.myRecievePin.write(0);
+            callback(null, 6);
+        },
+         function (callback) {
+
+            setTimeout(function () {
+                that.myRecievePin.dir(mraa.DIR_IN);
+                that.mySendPin.write(1);
+            }, 25);
+            callback(null, 7);
+        },
+         function (callback) {
+            setTimeout(function () {
+                console.log("SendStatus: " + that.mySendPin.read());
+                that.myRecievePin.dir(mraa.DIR_IN);
+                that.mySendPin.write(1);
+            }, 25);
+
+            callback(null, 8);
+        },
+        function (callback) {
+            console.log("timer: " + timer);
+            while(that.myRecievePin.read() === 0 && (timer < TIMEOUT_MS)) {
+            ++timer;
+            }
+            callback(null, 9);
+        },
+        function (callback) {
+            
+            if(timer >= TIMEOUT_MS) {
+                //return TIMEOUT;
+            }
+            
+            callback(null, 10);
+        },
+        function (callback) {
+            console.log("x");
+            that.myRecievePin.dir(mraa.DIR_OUT); // receivePin to OUTPUT - pin is now HIGH AND OUTPUT
+            callback(null, 11);
+        },
+        function (callback) {
+
+            that.myRecievePin.write(1);
+            callback(null, 12);
+        },
+        function (callback) {
+
+            that.myRecievePin.write(1);
+            callback(null, 13);
+        },
+        function (callback) {
+            console.log("x");
+            that.myRecievePin.write(0);
+            callback(null, 14);
+        },
+        function (callback) {
+
+            that.myRecievePin.dir(mraa.DIR_IN); // receivePin to INPUT (pullup is off)
+            callback(null, 15);
+        }
+    ],
+    function(err, results){
+
+        console.log(results);
+        
+        });
+
+    
+
+    return timer;
+};
+
+console.log("start");
+
+var sensori = new depth(46, 47);
+
+    console.log("valu: " + sensori.getValue());
