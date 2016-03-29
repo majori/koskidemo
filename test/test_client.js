2cdc10e23000ad7095f687100642a08311fcb094
Antti Kivimäki <majoriz11@gmail.com>
2016-03-29 12:03:17 +0300

    Split test environment to client and server side

--- test/test_client.js
+++ test/test_client.js
@@ -4,22 +4,26 @@ var chai            = require('chai');
 var chaiAsPromised  = require('chai-as-promised');
 var Promise         = require('bluebird');
 
-var client = require('../client/client');
-var server = require('../server/server');
-var logger = require('../config').logger
+var client      = require('../client/client');
+var database    = require('../client/database');
+var logger      = require('../config').logger;
 
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 var should = chai.should();
 
-describe('Client side', function() {
+describe('UDP', function() {
     it('can send UDP packet', function() {
         client.sendPacket(new Buffer('packet')).should.eventually.be.fulfilled;
     });
 });
 
-describe('Server side', function() {
-    it('can receive UDP packet', function() {
+describe('Database', function() {
+    before(function() {
+       // Clean test database
+    });
+
+    it('can register new guild', function() {
 
     });
 });
---
+++ test/test_server.js
@@ -0,0 +1,18 @@
+process.env.NODE_ENV = 'test';
+
+var chai            = require('chai');
+var chaiAsPromised  = require('chai-as-promised');
+var Promise         = require('bluebird');
+
+var server = require('../server/server');
+var logger = require('../config').logger
+
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var should = chai.should();
+
+describe('UDP', function() {
+    it('can receive UDP packet', function() {
+
+    });
+});
